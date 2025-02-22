9135f86e1ae153f63c03923852b11e2ea33ef517
isa <rh23003@ues.edu.sv>
2025-02-22 11:41:16 -0600

    adding pepe

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test {
+    // main function
+  public static void main(String[] args)
+  {
+      System.out.println("Welcome");
+      System.out.println("my");
+      System.out.println("vro");
+      System.out.println("my");
+      System.out.println("vro");
+      
+
+  }
+}
